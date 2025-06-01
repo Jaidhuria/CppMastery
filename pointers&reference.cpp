1d6f03ea92fade3ba1b390f35e027863074ffdf4
Jai Dhuria <jaidhuria002@gmail.com>
2025-06-01 12:52:15 +0530

    Add files via upload

---
+++ pointers&reference.cpp
@@ -0,0 +1,14 @@
+//Reference
+#include<iostream>
+using namespace std;
+int main(){
+
+    int i = 10;
+    int*p = &i;
+    int**pt = &p;
+    int***ptr = &pt;
+    cout << "i=" << i << "\t" << "p=" << p << "\t"
+
+<< "pt=" << pt << "\t" << "ptr=" << ptr << "\n";
+
+}
